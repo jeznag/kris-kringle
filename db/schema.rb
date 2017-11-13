6bac02b81c0159091cbdc0d004cd3e745193d01c
Jeremy Nagel <jnagel@cozero.com.au>
2017-11-14 08:41:53 +1100

    Fix height of disabled wrappers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171113201425) do
+ActiveRecord::Schema.define(version: 20171114083247) do
 
   create_table "accounts", force: :cascade do |t|
     t.string "account_name"
