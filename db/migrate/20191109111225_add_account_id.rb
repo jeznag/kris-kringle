fc09035b95f2ca5620cf0bb9660815261ce052bb
Jeremy Nagel <jeremymnagel@gmail.com>
2019-11-10 11:24:21 +1100

    try another approach

--- db/migrate/20191109111225_add_account_id.rb
+++ db/migrate/20191109111225_add_account_id.rb
@@ -1,7 +1,7 @@
 class AddAccountId < ActiveRecord::Migration[5.1]
   def up
-    change_column :accounts, :id, :primary_key
-    change_column :gift_exchanges, :id, :primary_key
-    change_column :family_members, :id, :primary_key
+    change_column :accounts, :id, :int, null: false, unique: true, auto_increment: true
+    change_column :gift_exchanges, :id, :int, null: false, unique: true, auto_increment: true
+    change_column :family_members, :id, :int, null: false, unique: true, auto_increment: true
   end
 end
