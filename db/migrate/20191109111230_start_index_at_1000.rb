b1b2c964933972aea5d6c701c5655d6802734b0a
Jeremy Nagel <jeremymnagel@gmail.com>
2019-11-10 11:45:05 +1100

    need a block

--- db/migrate/20191109111230_start_index_at_1000.rb
+++ db/migrate/20191109111230_start_index_at_1000.rb
@@ -1,7 +1,13 @@
 class StartIndexAt1000 < ActiveRecord::Migration[5.1]
   def up
-    change_table :accounts, :options => "AUTO_INCREMENT = 1000"
-    change_table :gift_exchanges, :options => "AUTO_INCREMENT = 1000"
-    change_table :family_members, :options => "AUTO_INCREMENT = 1000"
+    change_table :accounts, :options => "AUTO_INCREMENT = 1000" do |t|
+      # ...
+    end
+    change_table :gift_exchanges, :options => "AUTO_INCREMENT = 1000" do |t|
+      # ...
+    end
+    change_table :family_members, :options => "AUTO_INCREMENT = 1000" do |t|
+      # ...
+    end
   end
 end
