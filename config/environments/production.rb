c1b34b3dd3f027f1086cf1a8cc2f63b84725e7cd
Jeremy Nagel <jeremy.nagel@messagemedia.com.au>
2024-11-05 16:20:50 +1100

    Fix puma

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  # config/environments/production.rb
-
-  config.active_storage.service = :local
+  config.active_storage.service = :production
 end
